81ec5b0270acbf03ab1ec2784b74bc033a656022
xaos <xaos@xaos.tech>
2023-04-26 18:50:47 +0200

    doc: document Schema & TableList, seal TableList

--- src/schema.rs
+++ src/schema.rs
@@ -1,5 +1,9 @@
 use crate::Table;
 
+/// The set of [`Table`]s contained in a [`Database`](crate::Database)
+///
+/// Don't try to implement this trait manually -- use [`#[database]`](crate::database) on a tuple struct of [`Table`]s.
+/// The proc macro won't just generate the [`Schema`] implementation, it will also validate it.
 pub trait Schema {
 	type Tables: TableList;
 	type AllValues;
@@ -12,7 +16,14 @@ pub trait Schema {
 	}
 }
 
-pub trait TableList {
+/// Helper trait for implementing the [`Schema`]
+///
+/// This trait is sealed.
+/// It's implemented for nested tuples of [`Table`]s like `(TableA, (TableB, (TableC, )))` and so on.
+/// This theoretically allows for [`Schema`]s with any number (> 0) of [`Table`]s, though eventually you might compiler limits.
+///
+/// Again, don't bother implementing [`Schema`] manually, use [`#[database]`](crate::database).
+pub trait TableList: private::Sealed {
 	fn assemble_into(sql: &mut Vec<String>);
 }
 
@@ -26,4 +37,11 @@ impl<T: Table, L: TableList> TableList for (T, L) {
 		sql.push(T::assemble_sql());
 		L::assemble_into(sql)
 	}
+}
+
+mod private {
+	use super::*;
+	pub trait Sealed {}
+	impl<T: Table> Sealed for (T, ) {}
+	impl<T: Table, L: TableList> Sealed for (T, L) {}
 }
