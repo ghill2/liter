7d3eefe59efd2cbf8e265447798ae0d304569572
xaos <sr.ht@xaos.tech>
2023-12-09 22:16:58 +0100

    doc: document (most of) table.rs

--- src/table.rs
+++ src/table.rs
@@ -1,3 +1,5 @@
+//! SQL [`Table`]s defined at compile-time
+
 use construe::{
 	Construe,
 	StrConstrue,
@@ -21,33 +23,62 @@ use crate::value::{
 	StrChain
 };
 
+/// SQL table that can be used in a [`database`](crate::database)
+///
+/// The items that make up this trait are mostly an implementation detail.
+/// See the [`Entry`] trait (which is also implemented by the `#[derive(Table)]` proc-macro) as well as the [`HasKey`] trait (same, but only if the table has a primary key) for SQL generated to be used by you.
 pub trait Table {
+	/// Name of the table: `#[derive(Table)]` uses the lowercase name of the struct
 	const NAME: &'static str;
+	/// The [`TableDef`] struct defines the table and is used to assemble the `CREATE_TABLE` SQL statement
 	const DEFINITION: TableDef;
+	/// `CREATE TABLE` SQL statement
 	const CREATE_TABLE: &'static str;
 
+	/// Names of all the [`Column`](crate::Column)s that make up the table.
 	const ALL_COLUMNS: &'static [&'static str];
+	/// Names of the [`Column`](crate::Column)s that make up the values of the primary `#[key]`
+	///
+	/// This is empty if the [`Table`] has no primary key.
 	const KEY_COLUMNS: &'static [&'static str];
+	/// Names of all the [`Column`](crate::Column)s that make up the table, which *aren't* part of the primary key.
 	const OTHER_COLUMNS: &'static [&'static str];
 
+	/// Nested tuple that contains all the types (i.e. other tables) referenced (e.g. via [`Ref`]) by [`Value`](crate::Value)s in this table.
+	///
+	/// See the [`meta`](crate::meta) module for a little more detail on how this is used for [`Schema`](crate::schema) validation.
 	type References;
 }
 
-/// `'static' array of name & [`ValueDef`] pairs
+/// `'static` array of name & [`ValueDef`] pairs
 pub type Values = &'static [(&'static str, ValueDef)];
 
+/// Definition of a [`Table`] used to generate the SQL schema
+///
+/// Created by the `#[derive(Table)]` proc-macro.
 #[derive(Debug)]
 pub struct TableDef {
 	// TODO: "ON CONFLICT " clause
 	//on_conflict: ???,
+	/// Name of the table: `#[derive(Table)]` uses the lowercase name of the struct
 	pub name: &'static str,
+	/// Names of the [`Column`](crate::Column)s that make up the values of the primary `#[key]`
+	///
+	/// This is empty if the [`Table`] has no primary key.
 	pub primary_key:  &'static [&'static str],
+	/// Definitions and names for all constituent [`Value`](crate::Value)s
 	pub values: Values,
+	/// Definitions and names for all (if any) primary key [`Value`](crate::Value)s
 	pub key_values: Values,
+	/// Definitions and names for all [`Value`](crate::Value)s that are *not* (part of) the primary key.
+	///
+	/// This could be empty (if all [`Value']s are part of the primary key), or it could be equal to `values`.
 	pub other_values: Values,
+	/// List of [`Table`]-level [`Constraint`]s
 	pub constraints: &'static [Constraint],
 }
 
+/// SQL constraint at the [`Table`]-level
 #[derive(Debug)]
 pub enum Constraint {
 	/// SQL that will be put into a `CHECK (…)` constraint unmodified
@@ -56,8 +87,19 @@ pub enum Constraint {
 	Unique(Values)
 }
 
+/// SQL statements for interacting with a [`Table`]
+///
+/// This trait is also implemented by `#[derive(Table)]` and contains SQL statements to retrieve and insert entries from/into the table.
 pub trait Entry: Sized + Fetch + Bind {
+	/// `SELECT (...) FROM ...`
+	///
+	/// Select all [`Column`](crate::Column)s of all rows in this [`Table`].
+	/// [`Fetch::from_row`] can be used to convert the results of the query to this type.
 	const GET_ALL: &'static str;
+	/// `INSERT INTO ... VALUES ( ?, ...)`
+	///
+	/// Insert a new row into the [`Table`] for this type.
+	/// [`Bind`] is used to bind an instance of this type to the parameters in the correct order.
 	const INSERT: &'static str;
 }
 
@@ -68,6 +110,13 @@ pub trait HasKey {
 	/// Select an entry by its primary key.
 	/// Binds however many columns the key has.
 	const GET_BY_KEY: &'static str;
+	/// `INSERT INTO ... VALUES ( ?, ...) ON CONFLICT DO UPDATE SET (x = excluded.x)`\*
+	///
+	/// \*: Instead of `DO UPDATE SET (…)` it's `DO NOTHING` for key-only tables.
+	///
+	/// Be aware that this doesn't actually specify the "conflict target", that is, on violation of which uniqueness constraint to `DO UPDATE SET`, it simply *assumes* it is because of the primary key, *and* it will only update the non-key columns to the `excluded` values.
+	/// As such, you should probably not use this for tables with other `UNIQUE` constraints.  
+	/// See <https://sqlite.org/lang_upsert.html> for more on the "upsert" statement, which is not standard SQL.
 	const UPSERT: &'static str;
 	/// `UPDATE (...) SET (... = ?) WHERE (... = ?)`
 	///
@@ -94,7 +143,9 @@ pub trait HasKey {
 	}
 }
 
+/// Alias for [`HasKey`] restricted to non-composite primary keys
 pub trait HasSingleKey<K>: HasKey<Key = K, Marker = marker::One> {}
+/// Alias for [`HasKey`] restricted to composite primary keys
 pub trait HasCompositeKey<K>: HasKey<Key = K, Marker = marker::Many> {}
 
 impl<T: HasKey<Marker = marker::One>> HasSingleKey<T::Key> for T {}
@@ -188,6 +239,7 @@ impl Constraint {
 	}
 }
 
+/// Generates the [`HasKey::GET_BY_KEY`] statement at compile-time
 pub const fn get_by_key<const N: usize>(name: &str, key_columns: &[&str])
 	-> StrConstrue<N>
 {
@@ -210,6 +262,7 @@ pub const fn get_by_key<const N: usize>(name: &str, key_columns: &[&str])
 	sc.push_str(")")
 }
 
+/// Generates the [`HasKey::DELETE`] statement at compile-time
 pub const fn delete<const N: usize>(name: &str, key_columns: &[&str])
 	-> StrConstrue<N>
 {
@@ -229,6 +282,7 @@ pub const fn delete<const N: usize>(name: &str, key_columns: &[&str])
 	sc.push_str(")")
 }
 
+/// Generates the [`Entry::INSERT`] statement at compile-time
 pub const fn insert<const N: usize>(name: &str, column_count: usize)
 	-> StrConstrue<N>
 {
@@ -248,6 +302,7 @@ pub const fn insert<const N: usize>(name: &str, column_count: usize)
 	sc.push_str(")")
 }
 
+/// Generates the [`HasKey::UPSERT`] statement at compile-time
 pub const fn upsert<const N: usize>(
 	name: &str,
 	key_columns: &[&str],
@@ -300,6 +355,7 @@ pub const fn upsert<const N: usize>(
 	sc
 }
 
+/// Generates the [`HasKey::UPDATE`] statement at compile-time
 pub const fn update<const N: usize>(
 	name: &str,
 	val_is_key_and_count: &[(bool, usize)],
@@ -376,10 +432,12 @@ pub const fn update<const N: usize>(
 	sc
 }
 
+/// Helper struct to generate a slice of [`Column`](crate::Column) names at compile-time
 pub struct Names<const C: usize, const L: usize> {
 	bytes: StrConstrue<L>,
 	names: Construe<(usize, usize), C>
 }
+/// Helper struct that stores a buffer of [`Column`](crate::Column) names and an array of indices into it
 pub struct NameArrays<const C: usize, const L: usize> {
 	bytes: [u8; L],
 	names: [(usize, usize); C]
@@ -483,6 +541,7 @@ macro_rules! column_names {
 	}
 }
 
+/// Used by `#[derive(Table)]` to generate column name slices at compile time
 #[doc(inline)]
 pub use column_names;
 
